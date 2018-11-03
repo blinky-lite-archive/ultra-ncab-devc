b086d8b3c343a6ce7415c659b6b0972ed3748afc
David McGinnis <dmcginnis427@gmail.com>
2018-11-03 10:40:23 +0100

    changed name of device contrller

--- device-controller/device-controller.ino
+++ device-controller/device-controller.ino
@@ -1,52 +1,94 @@
-#include "device-controller.h"
+#include <ADC.h>
+#define PWMINBUFMAX 5
+const int pwmOutPin = 38;
+const int pwmInPin = 37;
+const int counterPin = 36;
+const int detPin = A0; // ADC0
 
-int loopCount = 0;
-int loopCountMax = 1000;
-float deltaMicros = 0;
+int pwMValue = 2048;
+int pwmFrequency = 40250;
+int pwmResolution = 12;
+
+ADC *adc = new ADC(); // adc object
+float detPinValue = 0;
+float avgDetPinValue = 0.0;
+float nsamples = 500.0;
+unsigned long lastWriteTime = 0;
 unsigned long nowTime;
-unsigned long loopStartTime;
-unsigned long startTime1Hz;
-boolean blinky = false;
-int led13Pin = 13;
-boolean led13 = false;
+float pwmInI;
+float pwmInQ;
+float overlapI = 0;
+float overlapQ = 0;
 
-void setup()
+boolean counterPinValue = false;
+int counter = 0;
+int maxCounter = 10;
+int deltaT;
+
+int pwmInBuf[PWMINBUFMAX];
+int pwmBufPointerI = PWMINBUFMAX - 1;
+int pwmBufPointerQ = 0;
+
+void setup() 
 {
-  setupCommunications(false, 9600);
-  pinMode(led13Pin, OUTPUT);     
-//  Serial.begin(9600);
-   
-  nowTime = micros();
-  loopStartTime = nowTime;
-  startTime1Hz = nowTime;
-  delay(1000);
+  for (int ii = 0; ii < PWMINBUFMAX; ++ii) pwmInBuf[ii] = 0;
+  lastWriteTime = millis();
+  analogWriteResolution(pwmResolution);   
+  analogWriteFrequency(pwmOutPin, pwmFrequency);  
+  pinMode(pwmOutPin, OUTPUT);  
+  pinMode(pwmInPin, INPUT);  
+  pinMode(counterPin, OUTPUT);  
+
+
+  analogWrite(pwmOutPin, pwMValue);
+  pinMode(detPin, INPUT);
+  adc->setAveraging(1); // set number of averages
+  adc->setResolution(12); // set bits of resolution
+//  adc->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_HIGH_SPEED); // change the conversion speed
+//  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED); // change the sampling speed
+//  adc->enableInterrupts(ADC_0);
+  adc->startContinuous(detPin, ADC_0);
+  Serial.begin(115200);
+
 }
 
-void loop()
+void loop() 
 {
-  if (dataOnSerial())
+//  delayMicroseconds(1);
+  detPinValue = (float) adc->analogReadContinuous(ADC_0);
+  avgDetPinValue = avgDetPinValue + (detPinValue - avgDetPinValue) / nsamples;
+
+  
+  pwmInBuf[pwmBufPointerI] = digitalRead(pwmInPin);
+  pwmInI = (float) pwmInBuf[pwmBufPointerI];
+  pwmInQ = (float) pwmInBuf[pwmBufPointerQ];
+     
+  overlapI = overlapI + ((detPinValue * pwmInI) - overlapI) / nsamples;
+  overlapQ = overlapQ + ((detPinValue * pwmInQ) - overlapQ) / nsamples;
+
+  ++pwmBufPointerI;
+  ++pwmBufPointerQ;
+  if (pwmBufPointerI == PWMINBUFMAX) pwmBufPointerI = 0;
+  if (pwmBufPointerQ == PWMINBUFMAX) pwmBufPointerQ = 0;
+ 
+/*
+  ++counter;
+  if (counter >  maxCounter)
   {
-    if (getInputTopic().equals("getLoopTime"))
-    {
-      printMessage("loopTime", floatToString(deltaMicros,2));
-    }
+    digitalWrite(counterPin, counterPinValue);
+    counterPinValue = !counterPinValue;
+    counter = 0;
   }
-  ++loopCount;
-  nowTime = micros();
-  if (loopCount > loopCountMax)
-  {     
-      deltaMicros = (float)(nowTime - loopStartTime) / ((float)loopCountMax);
-      loopCount = 0;
-      loopStartTime = nowTime;
-  }
-  if ((nowTime - startTime1Hz) > 1000000)
+*/
+  nowTime = millis();
+  deltaT = (int) (nowTime - lastWriteTime);
+  if (deltaT > 1000)
   {
-    startTime1Hz = nowTime;
-    blinky = !blinky;
-    led13 = !led13;
-    digitalWrite(led13Pin, led13);
-    printMessage("loopTime", floatToString(deltaMicros,2));
-    printMessage("blinky", booleanToString(blinky));
+    lastWriteTime = nowTime;
+    Serial.print(avgDetPinValue);
+    Serial.print(" ");
+    Serial.print(overlapI);
+    Serial.print(" ");
+    Serial.println(overlapQ);
   }
 }
-
